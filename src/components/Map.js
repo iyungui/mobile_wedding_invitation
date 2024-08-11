eee9e270d0bfac608f6ff38cc4c35c12a715e0f1
iyungui <rsdbddml@gmail.com>
2024-08-11 19:31:37 +0900

    카카오맵 작업 중

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,48 @@
-// import React from 'react';
-// import { MapMarker } from 'react-kakao-maps-sdk';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
+const MapContainer = styled.div`
+  width: 100%;
+  height: 300px; /* 지도의 높이를 조정할 수 있습니다 */
+  margin-top: 20px;
+`;
 
-// const Map = () => (
-//   <Map
-//     center={{ lat: 33.5563, lng: 126.79581 }}
-//     style={{ width: "100%", height: "360px" }}
-//   >
-//     <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-//       <div style={{ color: "#000" }}>Hello World!</div>
-//     </MapMarker>
-//   </Map>
-// );
+const Map = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=YOUR_APP_KEY`;
+    script.async = true;
+    script.onload = () => {
+      const container = document.getElementById('map'); // 지도를 표시할 container
+      const options = {
+        center: new window.kakao.maps.LatLng(37.5665, 126.9780), // 지도의 중심좌표 (서울의 좌표 예시)
+        level: 3, // 지도의 확대 레벨
+      };
+      const map = new window.kakao.maps.Map(container, options);
 
+      const positions = [
+        {
+          title: '청담웨딩홀', 
+          latlng: new window.kakao.maps.LatLng(37.523123, 127.047417) // 웨딩홀 위치 (예시)
+        }
+      ];
 
-// export default Map;
+      const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png'; 
+      const imageSize = new window.kakao.maps.Size(24, 35);
+
+      positions.forEach(position => {
+        new window.kakao.maps.Marker({
+          map: map, // 마커를 표시할 지도
+          position: position.latlng, // 마커의 위치
+          title: position.title, // 마커의 타이틀
+          image: new window.kakao.maps.MarkerImage(imageSrc, imageSize), // 마커 이미지 
+        });
+      });
+    };
+    document.head.appendChild(script);
+  }, []);
+
+  return <MapContainer id="map"></MapContainer>;
+};
+
+export default Map;
