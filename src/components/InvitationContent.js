1e50f6e11c779696b480a6d15ef1a06aff7737e7
iyungui <rsdbddml@gmail.com>
2024-08-12 19:40:35 +0900

    account 주석 처리" g

--- src/components/InvitationContent.js
+++ src/components/InvitationContent.js
@@ -68,14 +68,7 @@ const InvitationContent = () => {
       >
         <RSVP />
       </Section>
-      {/* <Section
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5 }}
-      >
-        <Account />
-      </Section> */}
+
       <Section
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
