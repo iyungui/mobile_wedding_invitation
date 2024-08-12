5e171aaa3dd2c8263c758e954a4898e12ecc7edc
iyungui <rsdbddml@gmail.com>
2024-08-12 19:32:15 +0900

    resizing"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
@@ -5,19 +6,19 @@ import InvitationContent from './components/InvitationContent';
 import headerVideo from './assests/headerVideo.MP4'; // 동영상 파일 import
 
 const VideoSection = styled.section`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   overflow: hidden;
 `;
 
 const Video = styled.video`
   width: 100%;
-  height: auto;  /* 비율을 유지하면서 너비에 맞게 크기 조절 */
-  object-fit: cover;  /* 전체 화면을 채우도록 설정 */
+  height: 100%;
+  object-fit: cover;  // 전체 화면을 채우도록 설정
 `;
 
 const ContentSection = styled.section`
-
+  width: 100%;
   background-color: #f5f5f5;
 `;
 
--- src/components/InvitationContent.js
+++ src/components/InvitationContent.js
@@ -1,3 +1,5 @@
+
+// InvitationContent.js
 import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
@@ -15,14 +17,17 @@ const ContentContainer = styled.div`
   text-align: center;
   line-height: 2;
   width: 100%;
-  max-width: 1200px;  /* 큰 화면에서의 최대 폭 설정 */
+  max-width: 1000px;  /* 큰 화면에서의 최대 폭 설정 */
   margin: 0 auto;  /* 중앙 정렬 */
   font-family: ${fontFamily};
+  box-sizing: border-box;  /* padding과 width 계산을 일관되게 처리 */
 `;
 
 const Section = styled(motion.div)`
   padding: 40px 0;
   text-align: center;
+  width: 100%;
+  box-sizing: border-box;  /* padding과 width 계산을 일관되게 처리 */
   @media (max-width: 768px) {
     padding: 20px 0;  /* 작은 화면에서는 패딩을 줄임 */
   }
